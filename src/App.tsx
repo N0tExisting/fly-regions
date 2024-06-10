619cafbed3a200ebd5088579c660ef60e480f4cf
N0tExisting <61290107+N0tExisting@users.noreply.github.com>
2024-06-10 18:24:15 +0200

    Add Suspense boundary for the map

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
+import Loader from "./components/Loader";
 import Map from "./components/Map";
 
 function App() {
-  return <Map />;
+  return (
+    <Loader>
+      <Map />
+    </Loader>
+  );
 }
 
 export default App;
